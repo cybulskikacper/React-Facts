d25cbb0d61fa891dabf8cdac681ee7d16aca8458
Kacper <kacpercybulski@icloud.com>
2023-08-28 00:04:08 +0200

    .

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Nav from './components/Navbar'
+import Main from './components/Main'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+	return (
+		<>
+			<Nav />
+			<Main />
+		</>
+	)
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Nav() {
+	return (
+		<>
+			<h1>Navbar goes here</h1>
+		</>
+	)
+}
+
+export default Nav
