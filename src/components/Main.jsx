e0c97dba316da4c00f73448438064c6812494e4b
Kacper <kacpercybulski@icloud.com>
2023-08-29 15:53:21 +0200

    My first React project is done

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 
 function Main() {
 	return (
-		<>
-			<h1>Main goes here</h1>
-		</>
+		<main>
+			<h1 className="title">Fun facts about React</h1>
+			<ul className="facts">
+				<li>Was first released in 2013</li>
+				<li>Was orginally created by Jordan Walke</li>
+				<li>Has well over 100K stars on GitHub</li>
+				<li>Is maintained by Facebook</li>
+				<li>Powers thousands of enteprise apps, including mobile apps </li>
+			</ul>
+		</main>
 	)
 }
 
